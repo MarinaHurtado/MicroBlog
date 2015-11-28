44b7d6bb7faa27d1ddb41c5d690a16ff5849780e
marinahurtado <mmhhvv1@gmail.com>
2015-11-28 18:59:26 +0000

    Agregamos modelo micropost

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,9 @@
                  password: password,
                  password_confirmation: password)
 end
+
+users = User.order(:created_at).take(6)
+50.times do |n|
+   content = Faker::Lorem.sentence(5)
+   users.each { |user| user.microposts.create!(content: content) }
+end
---
+++ test/models/micropost_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  #test "order should be most recent first" do
+   # assert_equal microposts(:most_recent), Micropost.first
+  #end
+end
